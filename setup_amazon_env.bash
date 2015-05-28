1f30fbfc1d4d4c188498126692462b1fea893a1f
Mark Vernon <vernon@mesatrail.com>
2015-05-28 13:58:27 -0500

    Refactored amazon for #1 and #6

--- setup_amazon_env.bash
+++ setup_amazon_env.bash
@@ -1,4 +1,3 @@
-export AWS_ACCESS_KEY=`grep -m 1 "aws_access_key:" playbook_vars/aws_secret_vars.yml | cut -d '"' -f 2`
-export AWS_SECRET_KEY=`grep -m 1 "aws_secret_key:" playbook_vars/aws_secret_vars.yml | cut -d '"' -f 2`
-export AWS_ACCESS_KEY_ID="${AWS_ACCESS_KEY}"
-export AWS_SECRET_ACCESS_KEY="${AWS_SECRET_KEY}"
+#!/usr/bin/env bash
+export AWS_ACCESS_KEY_ID=`grep -m 1 aws_access_key playbook_vars/aws_secret_vars.yml | cut -d '"' -f 2`
+export AWS_SECRET_ACCESS_KEY=`grep -m 1 aws_secret_key playbook_vars/aws_secret_vars.yml | cut -d '"' -f 2`
