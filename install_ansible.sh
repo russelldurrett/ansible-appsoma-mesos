c288b6e1a3848fecbe6308afc5e5416da911cd06
Mark Vernon <vernon@mesatrail.com>
2015-08-17 14:50:21 -0500

    fixed setup script

--- install_ansible.sh
+++ install_ansible.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
-sudo apt-get install software-properties-common
-sudo apt-add-repository ppa:ansible/ansible
+sudo apt-get install -y software-properties-common
+sudo apt-add-repository -y ppa:ansible/ansible
 sudo apt-get update
-sudo apt-get install ansible
+sudo apt-get install -y ansible
